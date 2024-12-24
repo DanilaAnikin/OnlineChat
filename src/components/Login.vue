43b54fcb21447f869573e59c2852151fabd0df15
Danila Anikin <danila.s.anikin@gmail.com>
2024-12-24 23:41:36 +0100

    .

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,6 +4,8 @@ import {supabase} from '../supabase'
 
 const username = ref("")
 const password = ref("")
+const email = ref('')
+
 
 
 const emits = defineEmits(['login', 'goSignup'])
@@ -11,6 +13,13 @@ const emits = defineEmits(['login', 'goSignup'])
 const login = async() => {
     const {data} = await supabase.from('users').select().eq('name', username.value).eq('password', password.value)
     emits('login', data[0].id)
+}
+
+const logIn = async() => {
+    const { data } = supabase.auth.signInWithPassword({
+        email: email.value,
+        password: password.value
+    })
 }
 
 </script>
@@ -21,8 +30,8 @@ const login = async() => {
     </div>
     <div class="mx-auto mt-8 bg-gray-800 max-w-2xl rounded-2xl p-14">
         <span class="text-3xl text-slate-100 font-bold">Login</span>
-        <label for="username" class="flex text-slate-100 mt-10">Username</label>
-        <input v-model="username" id="password" class="bg-transparent w-full mt-1 p-3 border border-green-500 rounded-lg focus:text-black text-slate-100 focus:bg-blue-50 focus:placeholder-green-600 placeholder-slate-400" placeholder="Username..."/>
+        <label for="email" class="flex text-slate-100 mt-10">Email</label>
+        <input v-model="email" class="bg-transparent w-full mt-1 p-3 border border-green-500 rounded-lg focus:text-black text-slate-100 focus:bg-blue-50 focus:placeholder-green-600 placeholder-slate-400" placeholder="Enter your email"/>
         <label for="password" class="flex text-slate-100 mt-5">Password</label>
         <input v-model="password" v-on:keyup.enter="login" type="password" id="password" class="w-full mt-1 p-3 border border-green-500 rounded-lg focus:text-black text-slate-100 focus:bg-blue-50 focus:placeholder-green-600 bg-transparent placeholder-slate-400" placeholder="Password..."/>
         <button class="mt-10 ml-96 text-white bg-green-600 w-32 h-12 rounded-lg font-bold text-lg" @click="login">Login</button>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -9,7 +9,7 @@ const secPassword = ref('')
 const passNotSame = ref(false)
 const blank = ref(false)
 const signupped = ref(false)
-
+const email = ref('')
 
 const emits = defineEmits(['goLogin'])
 
@@ -29,6 +29,18 @@ const signUp = async() => {
     }
 }
 
+const signup = async() => {
+    const { data } = supabase.auth.signUp({
+        email: email.value,
+        password: password.value,
+        options: {
+            data:{
+                username: username.value
+            }
+        }
+    })
+}
+
 
 </script>
 
@@ -39,12 +51,18 @@ const signUp = async() => {
     <div class=" mx-auto bg-gray-800 max-w-2xl rounded-2xl p-14 mt-8">
         <span class="text-3xl text-slate-100 font-bold">SignUp</span>
         <label for="username" class="flex text-slate-100 mt-10">Username</label>
-        <input v-model="username" id="password" class="bg-transparent w-full mt-1 p-3 border border-green-500 rounded-lg focus:text-black text-slate-100 focus:bg-blue-50 focus:placeholder-green-600 placeholder-slate-400" placeholder="Username..."/>
+        <input v-model="username" class="bg-transparent w-full mt-1 p-3 border border-green-500 rounded-lg focus:text-black text-slate-100 focus:bg-blue-50 focus:placeholder-green-600 placeholder-slate-400" placeholder="Enter your username"/>
+        
+        <label for="email" class="flex text-slate-100 mt-10">Email</label>
+        <input v-model="email" class="bg-transparent w-full mt-1 p-3 border border-green-500 rounded-lg focus:text-black text-slate-100 focus:bg-blue-50 focus:placeholder-green-600 placeholder-slate-400" placeholder="Enter your email"/>
+        
         <label for="password" class="flex text-slate-100 mt-5">Password</label>
-        <input v-model="password" type="password" id="password" :class="`${passNotSame ? 'bg-red-400' : 'bg-transparent'} w-full mt-1 p-3 border border-green-500 rounded-lg focus:text-black text-slate-100 focus:bg-blue-50 focus:placeholder-green-600 bg-transparent placeholder-slate-400`" placeholder="Password..."/>
+        <input v-model="password" type="password" id="password" :class="`${passNotSame ? 'bg-red-400' : 'bg-transparent'} w-full mt-1 p-3 border border-green-500 rounded-lg focus:text-black text-slate-100 focus:bg-blue-50 focus:placeholder-green-600 bg-transparent placeholder-slate-400`" placeholder="Enter your password"/>
+    
         <label for="password" class="flex text-slate-100 mt-5">Password again</label>
-        <input v-model="secPassword" type="password" id="password2" :class="`${passNotSame ? 'bg-red-400' : 'bg-transparent'} w-full mt-1 p-3 border border-green-500 rounded-lg focus:text-black text-slate-100 focus:bg-blue-50 focus:placeholder-green-600 bg-transparent placeholder-slate-400`" placeholder="One more time please..."/>
-        <span v-if="passNotSame" class="text-red-500 text-xl">Passwords aren't same</span>
+        <input v-model="secPassword" type="password" id="password2" :class="`${passNotSame ? 'bg-red-400' : 'bg-transparent'} w-full mt-1 p-3 border border-green-500 rounded-lg focus:text-black text-slate-100 focus:bg-blue-50 focus:placeholder-green-600 bg-transparent placeholder-slate-400`" placeholder="One more time"/>
+    
+        <span v-if="passNotSame" class="text-red-500 text-lg">Passwords aren't same</span>
         <span v-if="blank" class="text-red-500 text-xl">It can't be blank</span>
         <button class="mt-10 ml-96 text-white bg-green-600 w-32 h-12 rounded-lg font-bold text-lg" @click="signUp">SignUp</button>
     </div>
